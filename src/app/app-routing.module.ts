d0c0b567d0891d05db4a73fd76c1bb8b779ad895
riskers <gaoyibobobo@gmail.com>
2018-12-11 22:03:07 +0800

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { TestComponent } from './pages/test/test.component';
+
+const routes: Routes = [
+  { path: '',  redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'test', component: TestComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './components/list/list.component';
 import { LoadingComponent } from './components/loading/loading.component';
 import { GithubService } from './services/github.service';
+import { TestComponent } from './pages/test/test.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { GithubService } from './services/github.service';
     HomeComponent,
     ListComponent,
     LoadingComponent,
+    TestComponent,
   ],
   imports: [
     BrowserModule,
